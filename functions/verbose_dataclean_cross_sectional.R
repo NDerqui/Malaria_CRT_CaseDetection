c92fca44ecaeedf3029eaa1e2e6c2420a6a34efd
Nieves Derqui <n.derqui-fernandez@imperial.ac.uk>
2026-03-18 13:56:34 +0100

    Notation

--- functions/verbose_dataclean_cross_sectional.R
+++ functions/verbose_dataclean_cross_sectional.R
@@ -1,6 +1,8 @@
 
-# This function can be run at any time after a verbose sim:
-# before or after getting the age cohort.
+# INPUT: a df result of a verbose simulation, or from an age-cohort df.
+# OUTPUT: a df with only the timesteps "observed" with a cross sectional survey.
+
+# DESCRIPTION:
 
 # The function subsets timesteps to those "observed" in a cross sectional survey.
 # Idea being to then apply any of the other functions to detect infection or clinical cases.
