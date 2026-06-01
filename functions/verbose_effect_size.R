2ca0f5000f71a6fc5d88706b17a602e3a4ac826c
Nieves Derqui <n.derqui-fernandez@imperial.ac.uk>
2026-06-01 15:33:00 +0100

    Notation, wrap up for analysis function

--- functions/verbose_effect_size.R
+++ functions/verbose_effect_size.R
@@ -1,44 +1,61 @@
 
-# INPUT: trial outcome estimates with control and intervention arms.
+# INPUT: trial outcome estimates (prevalence, incidence, etc.).
 # OUTPUT: effect-size estimates comparing intervention with control.
+
 
 # DESCRIPTION:
 
-# Functions to estimate intervention effect sizes from trial outcomes.
-# Prevalence, incidence, and time-to-event functions estimate arm-specific
-# outcomes first. These functions perform the final intervention comparison.
+# Functions to estimate intervention effect sizes from trial outcomes:
+# Prevalence, incidence and time-to-event.
 
+
+# WRAP FOR PLOTTING AND EFFECT
+
+# For ease later, use a function to wrap all the different outputs 
+# (prevalence, incidence, whether true or observed by cross-sectional surveys, etc.),
+# which is useful for plotting too.
+
+wrap_for_plot_effect <- function(df) {
+  
+  require(dplyr)
+  
+  df <- df %>%
+    select(-c(n, person_days_at_risk, infections, cases, new_infections, new_cases, period, period_label)) %>%
+    pivot_longer(-c(timestep, type_measure, run), names_to = "measure", values_to = "value") %>%
+    mutate(measure = factor(measure, levels = measures, labels = measures_labels))
+  
+  return(df)
+  
+}
 
 # RELATIVE EFFECT SIZE
 
-# INPUT: a tidy outcome df with one value per arm and time/period.
-# OUTPUT: a tidy df with intervention effect estimated as 1 - intervention/control.
+# Getting the effect size of an intervention as: 1 - intervention/control.
 
 get_relative_effect <- function(df,
-                                value_col = "value",
-                                arm_col = "run",
-                                control = "Control",
-                                intervention = "Intervention",
-                                outcome_cols = c("measure"),
-                                comparison_cols = NULL) {
+                                value_col = "value", arm_col = "run",
+                                control = "Control", intervention = "Intervention",
+                                outcome_cols = c("type_measure", "measure", "timestep")) {
 
   require(dplyr)
   require(tidyr)
+  
+  # Make R recognise our columns for the numbers to compare and trial arms
 
   value_col <- rlang::sym(value_col)
   arm_col <- rlang::sym(arm_col)
 
-  df <- df %>%
-    select(timestep, all_of(c(comparison_cols, outcome_cols)), arm = !!arm_col, value = !!value_col) %>%
-    filter(arm %in% c(control, intervention)) %>%
+  result <- df %>%
+    # Select the cols we are interested in
+    select(all_of(outcome_cols), arm = !!arm_col, value = !!value_col) %>%
+    # Pivot to compare each
     tidyr::pivot_wider(names_from = arm, values_from = value) %>%
     mutate(
       effect = 1 - .data[[intervention]] / .data[[control]]
     )
   
-  return(df)
+  return(result)
 }
-
 
 # HAZARD RATIO EFFECT SIZE
 
